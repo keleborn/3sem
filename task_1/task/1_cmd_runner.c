065c308f1982128d9d00afa3581a572648c24fe8
keleborn <Ivanenko-Aleksei@ynadex.ru>
2017-09-23 09:08:17 +0300

    The task was done

--- task_1/task/1_cmd_runner.c
+++ task_1/task/1_cmd_runner.c
@@ -9,3 +9,48 @@
    a) if execvp is used, string splitting into "path" and "args" is all you need.
 3. Collect exit codes via waitpid/WEXITSTATUS.
 */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define ArgNumb 55
+
+int main(){
+	char S[256] = "";
+	do{
+	printf("To exit the programme enter 'quit'\n");
+	char *arg[ArgNumb];
+	char s[256];
+	fgets(S, 255, stdin);	
+	int counter = 0;
+	int i = 0;
+	do{
+		sscanf(S, "%s", s);
+		arg[counter] = s;
+		counter++;
+		i += strlen(s);
+	}while(i <= strlen(S));
+	int status;
+		pid_t pid = fork();
+		switch(pid){
+		default:
+			waitpid(pid, &status, 0);
+			printf("Ret code: %d\n", WEXITSTATUS(status));
+			break;
+		case -1:
+			printf("Error (fork)\n");
+			exit(-1);
+			break;
+		case 0:
+			execv(arg[0], arg+1);
+			printf("Error (exec)\n");
+			break;
+	}	
+}while(strcmp(S, "quit\n"));
+return 0;
+}
